61ab194b2ed8230e2e3b905d360e75e1b4fcda0e
Emmanuel okon <emmanuelokon591@gmail.com>
2024-06-18 22:55:22 +0100

    loaded fonts

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,9 +1,38 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { Slot, Stack } from "expo-router";
+import { Stack, SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    PoppinsBlack: require("../assets/fonts/Poppins-Black.ttf"),
+    PoppinsBold: require("../assets/fonts/Poppins-Bold.ttf"),
+    PoppinsExtraBold: require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    PoppinsExtraLight: require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    PoppinsLight: require("../assets/fonts/Poppins-Light.ttf"),
+    PoppinsMedium: require("../assets/fonts/Poppins-Medium.ttf"),
+    PoppinsRegular: require("../assets/fonts/Poppins-Regular.ttf"),
+    PoppinsSemiBold: require("../assets/fonts/Poppins-SemiBold.ttf"),
+    PoppinsThin: require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+
+  // useEffect(() => {
+  //   if (error) throw error;
+  //   if (fontsLoaded) throw SplashScreen.hideAsync();
+  // }, [fontsLoaded, error]);
+
+  // if (!fontsLoaded && !error) throw error;
+
+  if (!fontsLoaded && !error) {
+    return (
+      <View>
+        <Text>loading fonts</Text>
+      </View>
+    );
+  } else {
+    SplashScreen.hideAsync();
+  }
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
